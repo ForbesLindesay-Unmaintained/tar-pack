b48922d5d0863af280ed6197a0058ecf3b4b3fc3
Simon Nord <simon@nord.org>
2016-06-22 17:49:48 +0200

    Added tests for strip 0 and fromBase true options

--- test/index.js
+++ test/index.js
@@ -24,6 +24,16 @@ describe('tarball.pipe(unpack(directory, callback))', function () {
     }))
   })
 })
+describe('tarball.pipe(unpack(directory, { strip: 0 }, callback))', function () {
+  it('unpacks the tarball into the directory with subdir package', function (done) {
+    read(__dirname + '/fixtures/packed.tar').pipe(tar.unpack(__dirname + '/output/unpacked', { strip: 0 } , function (err) {
+      if (err) return done(err)
+      assert.equal(rfile('./output/unpacked/package/bar.txt'), rfile('./fixtures/to-pack/bar.txt'))
+      assert.equal(rfile('./output/unpacked/package/foo.txt'), rfile('./fixtures/to-pack/foo.txt'))
+      done()
+    }))
+  })
+})
 describe('gziptarball.pipe(unpack(directory, callback))', function () {
   it('unpacks the tarball into the directory', function (done) {
     read(__dirname + '/fixtures/packed.tar.gz').pipe(tar.unpack(__dirname + '/output/unpacked', function (err) {
@@ -57,6 +67,28 @@ describe('pack(directory).pipe(tarball)', function () {
         if (called) return
         called = true
         read(__dirname + '/output/packed.tar.gz').pipe(tar.unpack(__dirname + '/output/unpacked', function (err) {
+          if (err) return done(err)
+          assert.equal(rfile('./output/unpacked/bar.txt'), rfile('./fixtures/to-pack/bar.txt'))
+          assert.equal(rfile('./output/unpacked/foo.txt'), rfile('./fixtures/to-pack/foo.txt'))
+          done()
+        }))
+      })
+  })
+})
+describe('pack(directory, { fromBase: true }).pipe(tarball)', function () {
+  it('packs the directory with input dir as base dir', function (done) {
+    var called = false
+    mkdir(__dirname + '/output/')
+    tar.pack(__dirname + '/fixtures/to-pack', { fromBase: true }).pipe(write(__dirname + '/output/packed.tar.gz'))
+      .on('error', function (err) {
+        if (called) return
+        called = true
+        done(err)
+      })
+      .on('close', function () {
+        if (called) return
+        called = true
+        read(__dirname + '/output/packed.tar.gz').pipe(tar.unpack(__dirname + '/output/unpacked', { strip: 0 }, function (err) {
           if (err) return done(err)
           assert.equal(rfile('./output/unpacked/bar.txt'), rfile('./fixtures/to-pack/bar.txt'))
           assert.equal(rfile('./output/unpacked/foo.txt'), rfile('./fixtures/to-pack/foo.txt'))
